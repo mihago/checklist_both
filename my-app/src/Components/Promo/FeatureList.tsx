ea4cd82ddd7e9881bccaa1e46c437853a58a54ed
Savvin Michael <mihachess2004@gmail.com>
2025-05-18 17:05:29 +0300

    add ga

--- my-app/src/Components/Promo/FeatureList.tsx
+++ my-app/src/Components/Promo/FeatureList.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './FeatureList.css';
 
 const features = [
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import StepList from './Components/Promo/StepList.tsx'
-import FeatureList from './Components/Promo/FeatureList.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FeatureList />
+    <App />
   </StrictMode>,
 )
