a0053f45e0f2d7e0e0dc3838ad846f1c652d3126
Savvin Michael <mihachess2004@gmail.com>
2025-05-20 11:39:32 +0300

    add feedback link

--- my-app/src/Components/Header/Header.tsx
+++ my-app/src/Components/Header/Header.tsx
@@ -1,18 +1,26 @@
-
 import styles from "./Header.module.css";
 import Button from "../Button/Button";
 import DropdownMenu from "../DropdownMenu/DropdownMenu";
 
-interface HeaderProps{
-  saveChecklist:() => void,
-  token:string,
+interface HeaderProps {
+  saveChecklist: () => void;
+  token: string;
 }
-  const Header = ({saveChecklist,token}:HeaderProps) => {
-    
-    return <header className={styles.header}>
-       <Button color="#FFD966">Оставить фидбэк</Button>
-       <DropdownMenu saveChecklist = {saveChecklist} token={token}></DropdownMenu>
+const Header = ({ saveChecklist, token }: HeaderProps) => {
+  return (
+    <header className={styles.header}>
+      <Button color="#FFD966" className={styles.feedbackButton}>
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSerjFNlP8XTfvH781htmFql2vkdgzWaQai003UzZ9mAfMfJiA/viewform?usp=dialog"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Оставить фидбэк
+        </a>
+      </Button>
+      <DropdownMenu saveChecklist={saveChecklist} token={token}></DropdownMenu>
     </header>
-  };
-  
+  );
+};
+
 export default Header;
